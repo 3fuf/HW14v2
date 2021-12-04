60fcdefcb29f130ce0831c25a531fad4fac1467c
3fuf <91079054+3fuf@users.noreply.github.com>
2021-12-04 19:21:26 +0200

    add-js

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Test");
+
+const button = document.querySelector('.btn-burger');
+const navSite = document.querySelector('.nav-site');
+const close = document.querySelector('.nav-site-btn_close');
+
+button.addEventListener('click', () => {
+    navSite.classList.toggle('nav-site--open');
+});
+
+close.addEventListener('click', () => {
+    navSite.classList.remove('nav-site--open');
+});
